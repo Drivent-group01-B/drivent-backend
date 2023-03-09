2f8858ea6c501c69ea4682ad0e12b0a17ef35347
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-03-09 17:54:09 -0300

    refactor: return hotel with rooms and booking count

--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -4,19 +4,36 @@ async function findHotels() {
   return prisma.hotel.findMany({
     include: {
       Rooms: true,
-    }
+    },
   });
 }
 
 async function findRoomsByHotelId(hotelId: number) {
-  return prisma.hotel.findFirst({
-    where: {
-      id: hotelId,
+  // return prisma.hotel.findFirst({
+  //   where: {
+  //     id: hotelId,
+  //   },
+  //   include: {
+  //     Rooms: true,
+  //   },
+  // });
+
+  const rooms = await prisma.room.findMany({
+    where: { Hotel: { id: hotelId } },
+    select: {
+      id: true,
+      name: true,
+      hotelId: true,
+      capacity: true,
+      _count: { select: { Booking: true } },
     },
-    include: {
-      Rooms: true,
-    }
   });
+
+  const hotel = await prisma.hotel.findUnique({
+    where: { id: hotelId },
+  });
+
+  return { hotel, rooms: rooms };
 }
 
 const hotelRepository = {
