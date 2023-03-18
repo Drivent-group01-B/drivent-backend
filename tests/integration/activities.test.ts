3ebab795a4a0458f2f0944d05db4276d71f37e12
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-03-18 17:36:59 -0300

    feat: activities integration test

--- tests/integration/activities.test.ts
+++ tests/integration/activities.test.ts
@@ -2,14 +2,17 @@ import app, { init } from "@/app";
 import { cleanDb, generateValidToken } from "../helpers";
 import httpStatus from "http-status";
 import supertest from "supertest";
-import { TicketStatus } from "@prisma/client";
+import { Activity, TicketStatus } from "@prisma/client";
 import {
   createUser,
   createEnrollmentWithAddress,
   createPayment,
   createTicket,
   createTicketTypeWithHotel,
+  createEvent,
 } from "../factories";
+import { createActivity, createLocation } from "../factories/activities-factory";
+import dayjs from "dayjs";
 
 const server = supertest(app);
 
@@ -27,17 +30,42 @@ describe("GET /activities", () => {
 
   describe("When token is valid", () => {
     it("should respond with activities data given a date", async () => {
+      const event = await createEvent();
+
       const user = await createUser();
       const token = await generateValidToken(user);
-      // const enrollment = await createEnrollmentWithAddress(user);
+      await createEnrollmentWithAddress(user);
+
+      const location = await createLocation();
+      const activity = await createActivity(10, event.id, location.id);
+      //NAO PODE VER SE N TEM TICKET
+      //NAO PODE VER SE N TEM TICKET PRESENCIAL
+      //NAO PODE VER SE ELE N FINALIZOU O PAGAMENTO
+
       // const ticketType = await createTicketTypeWithHotel();
       // const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
       // const payment = await createPayment(ticket.id, ticketType.price);
-      const date = "2023-03-23";
 
-      const response = await server.get(`/activities?date=${date}`).set("Authorization", `Bearer ${token}`);
+      const response = await server.get(`/activities?date=${"2023-03-18"}`).set("Authorization", `Bearer ${token}`);
 
-      expect(response.status).toBe(httpStatus.OK);
+      // console.log(dayjs(activity.start_at).startOf("day").toDate());
+
+      // expect(response.status).toBe(httpStatus.OK);
+      // expect(response.body).toEqual(
+      //   expect.arrayContaining([
+      //     expect.objectContaining({
+      //       id: expect.any(Number),
+      //       name: expect.any(String),
+      //       vacancies: expect.any(Number),
+      //       start_at: expect.any(String),
+      //       end_at: expect.any(String),
+      //       eventId: expect.any(Number),
+      //       locationId: expect.any(Number),
+      //       createdAt: expect.any(String),
+      //       updatedAt: expect.any(String),
+      //     }),
+      //   ]),
+      // );
     });
   });
 });
